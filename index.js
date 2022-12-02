9ae24bcd420e8a431b4989aa4bc9b94514a0a547
almiranteFuchs <brunofuchs2002@gmail.com>
2022-12-01 22:43:42 -0300

    feat: bash script injection via ssh

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 require('dotenv').config();
 
-const { NodeSSH } = require('node-ssh')
-const ssh = new NodeSSH();
+const fs = require('fs');
+const { readFileSync } = require('fs');
+const { Client } = require('ssh2');
 
 
-ssh.connect({
-    host: 'h52',
-    username: 'bfss21',
-    password: process.env.SECRET,
-}).then(function () {
-    console.log('connected');
+const conn = new Client();
 
-     // Command
-  ssh.execCommand('wall a').then(function(result) {
-    console.log('STDOUT: ' + result.stdout)
-    console.log('STDERR: ' + result.stderr)
-  })
+if (process.argv.length < 5) {
+  console.log('Usage: node index.js <username> <secret> <.sh file>');
+  process.exit(1);
+}
 
-}).catch(function (err) {
-    console.log(err);
+// Get all hosts from json as array
+const rawdata = fs.readFileSync('./hosts.json');
+let hosts = JSON.parse(rawdata).hosts;
+
+
+// Get username and secret from command line
+const username = process.argv[2];
+const secret = process.argv[3];
+
+// Get .sh command from file
+const command = readFileSync(process.argv[4], 'utf8');
+
+// For each host, connect and run command
+hosts.forEach(async host => {
+
+  conn.on('ready', () => {
+    console.log('Client :: ready');
+    conn.exec(command, (err, stream) => {
+      if (err) throw err;
+      stream.on('close', (code, signal) => {
+        console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+        conn.end();
+      }).on('data', (data) => {
+        console.log('STDOUT:\n ' + data);
+      }).stderr.on('data', (data) => {
+        console.log('STDERR:\n ' + data);
+      });
+    });
+  }).connect({
+    host: host,
+    username: username,
+    password: secret
+  });
 });
--- out.sh
+++ out.sh
@@ -1,1 +1,8 @@
+#this is but a exemple of a script that can be used to run a program
 echo "aa"
+echo "bb"
+cd ~
+mkdir test_dir
+cd test_dir
+echo "Damn bruh" > sensitive_data.txt
+cat sensitive_data.txt
